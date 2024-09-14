2190773da41f5bdbb7ac4e9e3fda74295bced5c5
Agoogaloo <57082475+agoogaloo@users.noreply.github.com>
2024-09-14 13:49:25 -0600

    Created Project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Raylib_cs;
+using YarEngine;
+using YarEngine.Inputs;
+
+class Program
+{
+	public static void Main()
+
+	{
+		GameBase.Init("Are you telling me a ray libed this game?");
+		/*InputHandler.AddButtonBind("Debug", KeyboardKey.Grave);*/
+		GameBase.Run();
+	}
+}
+
+
